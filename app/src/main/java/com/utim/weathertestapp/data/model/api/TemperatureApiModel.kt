ba27e8cd1333fcfee94a4edb3f261744acadc9b3
Andrey <nullnullru@gmail.com>
2020-09-13 18:58:42 +0300

    Some fixes

--- app/src/main/java/com/utim/weathertestapp/data/model/api/TemperatureApiModel.kt
+++ app/src/main/java/com/utim/weathertestapp/data/model/api/TemperatureApiModel.kt
@@ -2,6 +2,6 @@ package com.utim.weathertestapp.data.model.api
 
 import com.google.gson.annotations.SerializedName
 
-data class TemperatureApiResponse(
-    @SerializedName("Metric") val metric: MetricApiResponse
+data class TemperatureApiModel(
+    @SerializedName("Metric") val metric: TempUnitApiModel
 )
--- app/src/main/java/com/utim/weathertestapp/data/model/api/deserializers/CalendarDeserializer.kt
+++ app/src/main/java/com/utim/weathertestapp/data/model/api/deserializers/CalendarDeserializer.kt
@@ -1,4 +1,28 @@
 package com.utim.weathertestapp.data.model.api.deserializers
 
-class CalendarDeserializer {
+import com.google.gson.JsonDeserializationContext
+import com.google.gson.JsonDeserializer
+import com.google.gson.JsonElement
+import java.lang.reflect.Type
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
+
+class CalendarDeserializer : JsonDeserializer<Calendar>{
+
+    companion object {
+        val DATE_FORMAT: DateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ")
+    }
+
+    override fun deserialize(
+        json: JsonElement,
+        typeOfT: Type,
+        context: JsonDeserializationContext
+    ): Calendar {
+        val stringDateTime = json.asString
+        return Calendar.getInstance().apply {
+            time = DATE_FORMAT.parse(stringDateTime)!!
+        }
+    }
+
 }
